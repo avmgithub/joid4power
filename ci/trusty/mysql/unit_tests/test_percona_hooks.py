28a3010cf14b7e56dda808924202c66a42ec474a
root <root@opnfv2.pokcloud.com>
2016-03-04 11:04:30 -0600

    Changes made to mysql for percona DB to install properly

---
+++ ci/trusty/mysql/unit_tests/test_percona_hooks.py
@@ -0,0 +1,131 @@
+import mock
+import sys
+from test_utils import CharmTestCase
+
+sys.modules['MySQLdb'] = mock.Mock()
+import percona_hooks as hooks
+
+TO_PATCH = ['log', 'config',
+            'get_db_helper',
+            'relation_ids',
+            'relation_set',
+            'update_nrpe_config',
+            'get_iface_for_address',
+            'get_netmask_for_address',
+            'is_bootstrapped',
+            'is_sufficient_peers']
+
+
+class TestHaRelation(CharmTestCase):
+    def setUp(self):
+        CharmTestCase.setUp(self, hooks, TO_PATCH)
+
+    @mock.patch('sys.exit')
+    def test_relation_not_configured(self, exit_):
+        self.config.return_value = None
+
+        class MyError(Exception):
+            pass
+
+        def f(x):
+            raise MyError(x)
+        exit_.side_effect = f
+        self.assertRaises(MyError, hooks.ha_relation_joined)
+
+    def test_resources(self):
+        self.relation_ids.return_value = ['ha:1']
+        password = 'ubuntu'
+        helper = mock.Mock()
+        attrs = {'get_mysql_password.return_value': password}
+        helper.configure_mock(**attrs)
+        self.get_db_helper.return_value = helper
+        self.get_netmask_for_address.return_value = None
+        self.get_iface_for_address.return_value = None
+        self.test_config.set('vip', '10.0.3.3')
+        self.test_config.set('sst-password', password)
+
+        def f(k):
+            return self.test_config.get(k)
+
+        self.config.side_effect = f
+        hooks.ha_relation_joined()
+
+        resources = {'res_mysql_vip': 'ocf:heartbeat:IPaddr2',
+                     'res_mysql_monitor': 'ocf:percona:mysql_monitor'}
+        resource_params = {'res_mysql_vip': ('params ip="10.0.3.3" '
+                                             'cidr_netmask="24" '
+                                             'nic="eth0"'),
+                           'res_mysql_monitor':
+                           hooks.RES_MONITOR_PARAMS % {'sstpass': 'ubuntu'}}
+        groups = {'grp_percona_cluster': 'res_mysql_vip'}
+
+        clones = {'cl_mysql_monitor': 'res_mysql_monitor meta interleave=true'}
+
+        colocations = {'vip_mysqld': 'inf: grp_percona_cluster cl_mysql_monitor'}  # noqa
+
+        locations = {'loc_percona_cluster':
+                     'grp_percona_cluster rule inf: writable eq 1'}
+
+        self.relation_set.assert_called_with(
+            relation_id='ha:1', corosync_bindiface=f('ha-bindiface'),
+            corosync_mcastport=f('ha-mcastport'), resources=resources,
+            resource_params=resource_params, groups=groups,
+            clones=clones, colocations=colocations, locations=locations)
+
+    def test_resource_params_vip_cidr_iface_autodetection(self):
+        """
+        Auto-detected values for vip_cidr and vip_iface are used to configure
+        VIPs, even when explicit config options are provided.
+        """
+        self.relation_ids.return_value = ['ha:1']
+        helper = mock.Mock()
+        self.get_db_helper.return_value = helper
+        self.get_netmask_for_address.return_value = '20'
+        self.get_iface_for_address.return_value = 'eth1'
+        self.test_config.set('vip', '10.0.3.3')
+        self.test_config.set('vip_cidr', '16')
+        self.test_config.set('vip_iface', 'eth0')
+
+        def f(k):
+            return self.test_config.get(k)
+
+        self.config.side_effect = f
+        hooks.ha_relation_joined()
+
+        resource_params = {'res_mysql_vip': ('params ip="10.0.3.3" '
+                                             'cidr_netmask="20" '
+                                             'nic="eth1"'),
+                           'res_mysql_monitor':
+                           hooks.RES_MONITOR_PARAMS % {'sstpass': 'None'}}
+
+        call_args, call_kwargs = self.relation_set.call_args
+        self.assertEqual(resource_params, call_kwargs['resource_params'])
+
+    def test_resource_params_no_vip_cidr_iface_autodetection(self):
+        """
+        When autodetecting vip_cidr and vip_iface fails, values from
+        vip_cidr and vip_iface config options are used instead.
+        """
+        self.relation_ids.return_value = ['ha:1']
+        helper = mock.Mock()
+        self.get_db_helper.return_value = helper
+        self.get_netmask_for_address.return_value = None
+        self.get_iface_for_address.return_value = None
+        self.test_config.set('vip', '10.0.3.3')
+        self.test_config.set('vip_cidr', '16')
+        self.test_config.set('vip_iface', 'eth1')
+
+        def f(k):
+            return self.test_config.get(k)
+
+        self.config.side_effect = f
+        hooks.ha_relation_joined()
+
+        resource_params = {'res_mysql_vip': ('params ip="10.0.3.3" '
+                                             'cidr_netmask="16" '
+                                             'nic="eth1"'),
+                           'res_mysql_monitor':
+                           hooks.RES_MONITOR_PARAMS % {'sstpass': 'None'}}
+
+        call_args, call_kwargs = self.relation_set.call_args
+        self.assertEqual(resource_params, call_kwargs['resource_params'])
---
+++ ci/trusty/mysql/unit_tests/test_percona_utils.py
@@ -0,0 +1,233 @@
+import mock
+import os
+import unittest
+import tempfile
+import sys
+
+sys.modules['MySQLdb'] = mock.Mock()
+import percona_utils
+
+from test_utils import CharmTestCase
+
+
+class UtilsTests(unittest.TestCase):
+    def setUp(self):
+        super(UtilsTests, self).setUp()
+
+    @mock.patch("percona_utils.log")
+    def test_update_empty_hosts_file(self, mock_log):
+        map = {'1.2.3.4': 'my-host'}
+        with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
+            percona_utils.HOSTS_FILE = tmpfile.name
+            percona_utils.HOSTS_FILE = tmpfile.name
+            percona_utils.update_hosts_file(map)
+
+        with open(tmpfile.name, 'r') as fd:
+            lines = fd.readlines()
+
+        os.remove(tmpfile.name)
+        self.assertEqual(len(lines), 1)
+        self.assertEqual(lines[0], "%s %s\n" % (map.items()[0]))
+
+    @mock.patch("percona_utils.log")
+    def test_update_hosts_file_w_dup(self, mock_log):
+        map = {'1.2.3.4': 'my-host'}
+        with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
+            percona_utils.HOSTS_FILE = tmpfile.name
+
+            with open(tmpfile.name, 'w') as fd:
+                fd.write("%s %s\n" % (map.items()[0]))
+
+            percona_utils.update_hosts_file(map)
+
+        with open(tmpfile.name, 'r') as fd:
+            lines = fd.readlines()
+
+        os.remove(tmpfile.name)
+        self.assertEqual(len(lines), 1)
+        self.assertEqual(lines[0], "%s %s\n" % (map.items()[0]))
+
+    @mock.patch("percona_utils.log")
+    def test_update_hosts_file_entry(self, mock_log):
+        altmap = {'1.1.1.1': 'alt-host'}
+        map = {'1.1.1.1': 'hostA',
+               '2.2.2.2': 'hostB',
+               '3.3.3.3': 'hostC',
+               '4.4.4.4': 'hostD'}
+        with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
+            percona_utils.HOSTS_FILE = tmpfile.name
+
+            with open(tmpfile.name, 'w') as fd:
+                fd.write("#somedata\n")
+                fd.write("%s %s\n" % (altmap.items()[0]))
+
+            percona_utils.update_hosts_file(map)
+
+        with open(percona_utils.HOSTS_FILE, 'r') as fd:
+            lines = fd.readlines()
+
+        os.remove(tmpfile.name)
+        self.assertEqual(len(lines), 5)
+        self.assertEqual(lines[0], "#somedata\n")
+        self.assertEqual(lines[1], "%s %s\n" % (map.items()[0]))
+        self.assertEqual(lines[4], "%s %s\n" % (map.items()[3]))
+
+    @mock.patch("percona_utils.log")
+    @mock.patch("percona_utils.config")
+    @mock.patch("percona_utils.update_hosts_file")
+    @mock.patch("percona_utils.get_host_ip")
+    @mock.patch("percona_utils.relation_get")
+    @mock.patch("percona_utils.related_units")
+    @mock.patch("percona_utils.relation_ids")
+    def test_get_cluster_hosts(self, mock_rel_ids, mock_rel_units,
+                               mock_rel_get, mock_get_host_ip,
+                               mock_update_hosts_file, mock_config,
+                               mock_log):
+        mock_rel_ids.return_value = [1]
+        mock_rel_units.return_value = [2]
+        mock_get_host_ip.return_value = 'hostA'
+
+        def _mock_rel_get(*args, **kwargs):
+            return {'private-address': '0.0.0.0'}
+
+        mock_rel_get.side_effect = _mock_rel_get
+        mock_config.side_effect = lambda k: False
+
+        hosts = percona_utils.get_cluster_hosts()
+
+        self.assertFalse(mock_update_hosts_file.called)
+        mock_rel_get.assert_called_with(rid=1, unit=2)
+        self.assertEqual(hosts, ['hostA', 'hostA'])
+
+    @mock.patch("percona_utils.get_ipv6_addr")
+    @mock.patch("percona_utils.log")
+    @mock.patch("percona_utils.config")
+    @mock.patch("percona_utils.update_hosts_file")
+    @mock.patch("percona_utils.get_host_ip")
+    @mock.patch("percona_utils.relation_get")
+    @mock.patch("percona_utils.related_units")
+    @mock.patch("percona_utils.relation_ids")
+    def test_get_cluster_hosts_ipv6(self, mock_rel_ids, mock_rel_units,
+                                    mock_rel_get, mock_get_host_ip,
+                                    mock_update_hosts_file, mock_config,
+                                    mock_log, mock_get_ipv6_addr):
+        ipv6addr = '2001:db8:1:0:f816:3eff:fe79:cd'
+        mock_get_ipv6_addr.return_value = [ipv6addr]
+        mock_rel_ids.return_value = [1, 2]
+        mock_rel_units.return_value = [3, 4]
+        mock_get_host_ip.return_value = 'hostA'
+
+        def _mock_rel_get(*args, **kwargs):
+            return {'private-address': '0.0.0.0',
+                    'hostname': 'hostB'}
+
+        mock_rel_get.side_effect = _mock_rel_get
+        mock_config.side_effect = lambda k: True
+
+        hosts = percona_utils.get_cluster_hosts()
+
+        mock_update_hosts_file.assert_called_with({ipv6addr: 'hostA',
+                                                   '0.0.0.0': 'hostB'})
+        mock_rel_get.assert_called_with(rid=2, unit=4)
+        self.assertEqual(hosts, ['hostA', 'hostB'])
+
+    @mock.patch.object(percona_utils, 'log', lambda *args, **kwargs: None)
+    @mock.patch.object(percona_utils, 'related_units')
+    @mock.patch.object(percona_utils, 'relation_ids')
+    @mock.patch.object(percona_utils, 'config')
+    def test_is_sufficient_peers(self, mock_config, mock_relation_ids,
+                                 mock_related_units):
+        _config = {'min-cluster-size': None}
+        mock_config.side_effect = lambda key: _config.get(key)
+        self.assertTrue(percona_utils.is_sufficient_peers())
+
+        mock_relation_ids.return_value = ['cluster:0']
+        mock_related_units.return_value = ['test/0']
+        _config = {'min-cluster-size': 3}
+        self.assertFalse(percona_utils.is_sufficient_peers())
+
+        mock_related_units.return_value = ['test/0', 'test/1']
+        self.assertTrue(percona_utils.is_sufficient_peers())
+
+    @mock.patch.object(percona_utils, 'lsb_release')
+    def test_packages_gt_utopic(self, mock_lsb_release):
+        mock_lsb_release.return_value = {'DISTRIB_CODENAME': 'vivid'}
+        self.assertEqual(percona_utils.determine_packages(),
+                         ['percona-xtradb-cluster-server-5.6',
+                          'mysql-client-5.6'])
+
+    @mock.patch.object(percona_utils, 'lsb_release')
+    def test_packages_le_utopic(self, mock_lsb_release):
+        mock_lsb_release.return_value = {'DISTRIB_CODENAME': 'utopic'}
+        self.assertEqual(percona_utils.determine_packages(),
+                         ['percona-xtradb-cluster-server-5.5',
+                          'percona-xtradb-cluster-client-5.5'])
+
+    @mock.patch.object(percona_utils, 'get_wsrep_value')
+    def test_cluster_in_sync_not_ready(self, _wsrep_value):
+        _wsrep_value.side_effect = [None, None]
+        self.assertFalse(percona_utils.cluster_in_sync())
+
+    @mock.patch.object(percona_utils, 'get_wsrep_value')
+    def test_cluster_in_sync_ready_syncing(self, _wsrep_value):
+        _wsrep_value.side_effect = [True, None]
+        self.assertFalse(percona_utils.cluster_in_sync())
+
+    @mock.patch.object(percona_utils, 'get_wsrep_value')
+    def test_cluster_in_sync_ready_sync(self, _wsrep_value):
+        _wsrep_value.side_effect = [True, 4]
+        self.assertTrue(percona_utils.cluster_in_sync())
+
+    @mock.patch.object(percona_utils, 'get_wsrep_value')
+    def test_cluster_in_sync_ready_sync_donor(self, _wsrep_value):
+        _wsrep_value.side_effect = [True, 2]
+        self.assertTrue(percona_utils.cluster_in_sync())
+
+
+TO_PATCH = [
+    'status_set',
+    'is_sufficient_peers',
+    'is_bootstrapped',
+    'config',
+    'cluster_in_sync',
+]
+
+
+class TestAssessStatus(CharmTestCase):
+    def setUp(self):
+        CharmTestCase.setUp(self, percona_utils, TO_PATCH)
+
+    def test_single_unit(self):
+        self.config.return_value = None
+        self.is_sufficient_peers.return_value = True
+        percona_utils.assess_status()
+        self.status_set.assert_called_with('active', mock.ANY)
+
+    def test_insufficient_peers(self):
+        self.config.return_value = 3
+        self.is_sufficient_peers.return_value = False
+        percona_utils.assess_status()
+        self.status_set.assert_called_with('blocked', mock.ANY)
+
+    def test_not_bootstrapped(self):
+        self.config.return_value = 3
+        self.is_sufficient_peers.return_value = True
+        self.is_bootstrapped.return_value = False
+        percona_utils.assess_status()
+        self.status_set.assert_called_with('waiting', mock.ANY)
+
+    def test_bootstrapped_in_sync(self):
+        self.config.return_value = 3
+        self.is_sufficient_peers.return_value = True
+        self.is_bootstrapped.return_value = True
+        self.cluster_in_sync.return_value = True
+        percona_utils.assess_status()
+        self.status_set.assert_called_with('active', mock.ANY)
+
+    def test_bootstrapped_not_in_sync(self):
+        self.config.return_value = 3
+        self.is_sufficient_peers.return_value = True
+        self.is_bootstrapped.return_value = True
+        self.cluster_in_sync.return_value = False
+        percona_utils.assess_status()
+        self.status_set.assert_called_with('blocked', mock.ANY)
